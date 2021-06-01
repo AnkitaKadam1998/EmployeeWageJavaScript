365b83f9533392b15045c318ccf256dc13dbd8b1
komal@komal405.com <ankita80kadam@gmail.com>
2021-06-01 15:29:27 +0530

    ADD:Calculate wages till a condition of total hours

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -1,9 +1,11 @@
-const NUM_OF_WORKING_DAYS=20;
 const IS_PART_TIME=1;
 const IS_FULL_TIME=2;
 const PART_TIME_HOURS=4;
 const FULL_TIME_HOURS=8;
 const WAGE_PER_HOUR=20;
+const NUM_OF_WORKING_DAYS=20;
+const MAX_HRS_IN_MONTH=100;
+
 
 function getWorkingHours(empCheck)
 {
@@ -11,20 +13,25 @@ function getWorkingHours(empCheck)
 {
                 case IS_PART_TIME:
                         return PART_TIME_HOURS;
-                case IS_FULL_TIME:
+         break;
+      case IS_FULL_TIME:
                         return FULL_TIME_HOURS;
-                default:
-                        return 0;
+         break;
+        default:
+         return 0;
+         break;
 }
 }
-let empHrs=0;
-
-for(let day=0;day<NUM_OF_WORKING_DAYS;day++)
+let totalEmpHrs=0;
+let totalWorkingDays=0;
+while(totalWorkingDays<=NUM_OF_WORKING_DAYS && totalEmpHrs<NUM_OF_WORKING_DAYS)
 {
+        totalWorkingDays++;
         let empCheck=Math.floor(Math.random()*10)%3;
-        empHrs += getWorkingHours(empCheck);
+        totalEmpHrs +=getWorkingHours(empCheck);
 }
-let empWage = empHrs * WAGE_PER_HOUR;
-console.log("Total Hrs:" +empHrs+ "Emp Wage: "+empWage);
+let empWage=totalEmpHrs*WAGE_PER_HOUR;
+console.log("Total Days :"+totalWorkingDays+"Total Hrs:"+totalEmpHrs+"hrs Employee Wage:"+empWage);
+
 
 
